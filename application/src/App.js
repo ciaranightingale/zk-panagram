e4b3ea0d0857989fb72ba5b3c132fd511c49c2e7
Ciara Nightingale <ciara.nightingale@sky.com>
2025-01-19 21:45:27 +0000

    add zk into front-end

--- application/src/App.js
+++ application/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import PanagramInput from './PanagramInput';
 
+// import { compile, createFileManager } from "@noir-lang/noir_wasm"
+
+// import main from "../circuit/src/main.nr?url";
+// import nargoToml from "../circuit/Nargo.toml?url";
+
 function App() {
   return (
     <div className="App">
