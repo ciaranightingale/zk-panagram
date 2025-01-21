8f0d6198bca32d922e85beb083e3512212a0b6d2
Ciara Nightingale <ciara.nightingale@sky.com>
2025-01-21 16:03:51 +0000

    move out getCircuit func

--- vite/src/PanagramInput.tsx
+++ vite/src/PanagramInput.tsx
@@ -1,40 +1,13 @@
 import { useState } from 'react';
 import PanagramImage from "./PanagramImage";
 import { useAccount } from 'wagmi';
-import { compile, createFileManager } from "@noir-lang/noir_wasm";
-import main from "../../circuits/src/main.nr?url";
-import nargoToml from "../../circuits/Nargo.toml?url";
+
 import { UltraHonkBackend } from '@aztec/bb.js';
 import { Noir } from '@noir-lang/noir_js';
-import initNoirC from "@noir-lang/noirc_abi";
-import initACVM from "@noir-lang/acvm_js";
-import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
-import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
 import { useWriteContract, useWaitForTransactionReceipt } from 'wagmi';
 import {abi} from './abi/abi.ts';
 import ConnectWallet from './ConnectWallet';
-
-await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
-
-
-
-export async function getCircuit() {
-  const fm = createFileManager("/");
-  const mainResponse = await fetch(main);
-  const nargoTomlResponse = await fetch(nargoToml);
-
-  const mainBody = mainResponse.body;
-  const nargoTomlBody = nargoTomlResponse.body;
-
-  if (!mainBody || !nargoTomlBody) {
-    throw new Error("Failed to fetch required files.");
-  }
-
-  fm.writeFile("./src/main.nr", mainBody);
-  fm.writeFile("./Nargo.toml", nargoTomlBody);
-
-  return await compile(fm);
-}
+import { getCircuit } from './getCircuit';
 
 function PanagramInput() {
   const {data: isValid, isPending, writeContract} = useWriteContract();
---
+++ vite/src/getCircuit.ts
@@ -0,0 +1,28 @@
+import { compile, createFileManager } from "@noir-lang/noir_wasm";
+import main from "../../circuits/src/main.nr?url";
+import nargoToml from "../../circuits/Nargo.toml?url";
+
+import initNoirC from "@noir-lang/noirc_abi";
+import initACVM from "@noir-lang/acvm_js";
+import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
+import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
+
+await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
+
+export async function getCircuit() {
+    const fm = createFileManager("/");
+    const mainResponse = await fetch(main);
+    const nargoTomlResponse = await fetch(nargoToml);
+  
+    const mainBody = mainResponse.body;
+    const nargoTomlBody = nargoTomlResponse.body;
+  
+    if (!mainBody || !nargoTomlBody) {
+      throw new Error("Failed to fetch required files.");
+    }
+  
+    fm.writeFile("./src/main.nr", mainBody);
+    fm.writeFile("./Nargo.toml", nargoTomlBody);
+  
+    return await compile(fm);
+  }
